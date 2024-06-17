1d079792d10b9ff09a22a895d47ebc029828f4e0
Gilson Junio Sales Andrade <gilsonjunio@protonmail.com>
2024-06-16 23:44:06 -0300

    Novo arquivo

---
+++ aprendendo-python.py
@@ -0,0 +1,22 @@
+#Baixe o Python em https://python.org
+#Isso é um comentário em Python
+#Os comentários são ignorados pelo interpretador Python
+
+
+#Declaração de variáveis em Python
+variavel = "string" # essa é a declaração de variavel no python
+variavel_inteira = 1
+variavel_flutuante = 1.2 
+variavel_booleana = True
+
+#Tipos de dados em Python
+#String, Integer, Float e Boolean
+
+#Declaração de Funções em Python
+#
+#def function nome_funcao():
+#	código da função
+
+
+#Funções em Python
+print() # Função para printar argumentos na tela
